a614db16f89f345883b68ba2573d8286ff8e2b57
Mondeep Chetry <mandeepchetry6969@gmail.com>
2024-08-24 04:22:28 +0530

    added index.html

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 
 
 const express = require("express");
+const path = require("path");
 const app = express();
 const schoolRoutes = require("./routes/schoolRoutes");
 require('dotenv').config();
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.use("/api/v1", schoolRoutes);
 
