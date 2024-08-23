b13e4c37b759046f4a886b570285b8025f34f96c
Mondeep Chetry <mandeepchetry6969@gmail.com>
2024-08-23 22:29:30 +0530

    fixed env

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const sql = require('mysql2')
 
 const connection = sql.createConnection({
-    host: 'localhost',
-    database: 'SCHOOL',
-    user: 'root',
-    password: 'Gomagni@123'
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD
 })
 
 module.exports = connection;
