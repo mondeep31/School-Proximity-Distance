32e3e169229126118886b3e37c9eda9c2f3e70fb
Mondeep Chetry <mandeepchetry6969@gmail.com>
2024-08-25 11:56:52 +0530

    introduced a new middleware: validateCoordinates

--- middleware/validateSchoolData.js
+++ middleware/validateSchoolData.js
@@ -1,4 +1,4 @@
-const validateSchoolData = (req, res, next) => {
+const validateSchool = (req, res, next) => {
     const { name, address, latitude, longitude } = req.body;
 
     let errorMessage = "";
@@ -7,6 +7,32 @@ const validateSchoolData = (req, res, next) => {
         errorMessage = "Invalid Input: 'name' is required "
     } else if (!address) {
         errorMessage = "Invalid Input: 'address' is required "
+    } else if (!latitude) {
+        errorMessage = "Invalid Input: 'latitude' is required "
+    } else if (!longitude) {
+        errorMessage = "Invalid Input: 'longitude is required"
+    } else if (typeof latitude !== 'number') {
+        errorMessage = "Invalid Input: 'latitude' must be a number "
+    } else if (typeof longitude !== 'number') {
+        errorMessage = "Invalid Input: 'longitude' must be a number"
+    }
+
+    if (errorMessage) {
+        return res.status(400).json({ error: errorMessage });
+    }
+
+    next();
+}
+
+const validateCoordinates = (req, res, next) => {
+    const { latitude, longitude } = req.query;
+
+    let errorMessage = "";
+
+    if (!latitude) {
+        errorMessage = "Invalid Input: 'latitude' is required "
+    } else if (!longitude) {
+        errorMessage = "Invalid Input: 'longitude' is required "
     } else if (typeof latitude !== 'number') {
         errorMessage = "Invalid Input: 'latitude' must be a number "
     } else if (typeof longitude !== 'number') {
@@ -19,4 +45,4 @@ const validateSchoolData = (req, res, next) => {
     next();
 }
 
-module.exports = { validateSchoolData };
+module.exports = { validateSchool, validateCoordinates };
--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const schoolController = require("../controllers/schoolController");
-const { validateSchoolData } = require("../middleware/validateSchoolData");
+const { validateSchool, validateCoordinates } = require("../middleware/validateSchoolData");
 
-router.post("/addSchool", validateSchoolData, schoolController.addSchool);
-router.get("/listSchools", schoolController.listSchools);
+
+router.post("/addSchool", validateSchool, schoolController.addSchool);
+router.get("/listSchools", validateCoordinates, schoolController.listSchools);
 
 module.exports = router;
